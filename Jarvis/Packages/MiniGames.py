a467baa492c6e6856b0d0b15c430f248f2dc4ce0
DFaustino6 <a61171@ualg.pt>
2020-11-20 22:32:06 +0000

    rps chances

--- Jarvis/Packages/MiniGames.py
+++ Jarvis/Packages/MiniGames.py
@@ -4,5 +4,8 @@ class RPS:
     def jarvisChoice(self):
         random.choice("⛰","📰","✂")
 
-    def gameResult(self):
-        return
+    def gameResult(self,userChoice,jarvisChoice):
+        if userChoice == "⛰" and jarvisChoice == "✂" or userChoice == "📰" and jarvisChoice == "⛰" or userChoice == "✂" and jarvisChoice == "📰":
+            return "You won!"
+        elif userChoice == jarvisChoice:
+            return "It's a tie!"
