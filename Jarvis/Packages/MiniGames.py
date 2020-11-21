d95ac1c5d9402ab5a33957a66d58e6617b1162d7
DFaustino6 <a61171@ualg.pt>
2020-11-21 00:46:22 +0000

    rps done

--- Jarvis/Packages/MiniGames.py
+++ Jarvis/Packages/MiniGames.py
@@ -21,8 +21,7 @@ class RPS:
             await client.delete_message(message)
         else: 
             jarvisChoice=random.choice(["⛰","📰","✂"])
-            new_embed = discord.Embed(title=self.gameResult(reaction.emoji,jarvisChoice),color=0xa69ea8)
-            new_embed.add_field(name=reaction.emoji+" vs "+jarvisChoice,value=" ")
+            new_embed = discord.Embed(title=self.gameResult(reaction.emoji,jarvisChoice), description = reaction.emoji+" vs "+jarvisChoice,color=0xa69ea8)
             await rpsMsg.edit(embed=new_embed)
            
     def gameResult(self,userChoice,jarvisChoice):
@@ -30,24 +29,3 @@ class RPS:
             return "You won!"
         elif userChoice == jarvisChoice:
             return "It's a tie!"
-
-
-class coinFlip:
-    async def coinFlip(self,client,message):
-        choice = random.choice(["Heads!", "Tails!"])
-        embed = discord.Embed(title="Coin Flip!", description = "It was... " + choice ,color=0xa69ea8)
-        msgEmbed = await message.channel.send(embed=embed)
-        await msgEmbed.add_reaction("♻")
-
-        def checkCoinFlip(reaction,user):
-            return reaction.message.id == msgEmbed.id and user == message.author and reaction.emoji == "♻"
-
-        try:
-            reaction, user = await client.wait_for('reaction_add',timeout=30.0,check=checkCoinFlip)
-        except asyncio.TimeoutError:
-            pass
-        else:
-            await msgEmbed.delete() 
-            await self.coinFlip(client,message)
-        
-         
