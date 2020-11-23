e6918eece000e0864913b800d60262fe5230de99
6135 <guibscosta@hotmail.com>
2020-11-23 19:49:54 +0000

    Update jarvis.py

--- Jarvis/jarvis.py
+++ Jarvis/jarvis.py
@@ -9,7 +9,6 @@ from datetime import datetime
 from dotenv import load_dotenv
 from dotenv.main import find_dotenv
 from MiniGames import RPS, CoinFlip
-
 async def reactBack(client,message):
 	embed=discord.Embed(title="React to this message", color=0x80ff00)
 	embed.set_author(name="Jarvis - RB")
