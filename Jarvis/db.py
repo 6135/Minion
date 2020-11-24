ba98a0650d818216da2c76c959ec4a33e6bdddc1
6135 <guibscosta@hotmail.com>
2020-11-24 17:41:55 +0000

    Update db.py

--- Jarvis/db.py
+++ Jarvis/db.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from dotenv.main import find_dotenv
 import mysql.connector
+load_dotenv(find_dotenv())
 print(os.getenv("BOT_TOKEN"))
 print(os.getenv('DB_USER'))
 print(os.getenv('DB_PASSWORD'))
